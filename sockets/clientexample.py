70e6a638e2a2596cfd34cc5b31ed5ef31d679668
ManosFps1pt <manos.dragasakis@gmail.com>
2024-10-05 18:08:10 +0300

    changes

--- sockets/clientexample.py
+++ sockets/clientexample.py
@@ -6,6 +6,40 @@ nickname = input("Choose your nickname: ")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('dra-server.mywire.org', 55555))
+client.connect(("127.0.0.1", 55555))
 
 print(client.recv(1048).decode())
+
+
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode()
+            if message == 'NICK':
+                client.send(nickname.encode())
+            else:
+                print(message)
+        except socket.error:
+            # Close Connection When Error
+            print("An error occurred!")
+            client.close()
+            break
+
+
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode())
+
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
