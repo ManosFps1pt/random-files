082acf9557d8331345ebf342de1731c7eb8599e1
ManosFps1pt <manos.dragasakis@gmail.com>
2025-05-28 10:59:49 +0300

    changes

--- SymPy.py
+++ SymPy.py
@@ -3,7 +3,7 @@ from sympy import symbols, solve, Eq, oo, sqrt
 x = symbols("x")
 y = symbols("y")
 
-equation = solve(Eq(x, sqrt(16 ** 2 / 4 ** 2)))
+equation = solve(Eq(x + x ** 2 + y + y ** 2 * 69, 666), x)
 
 print(f"{equation=}")
 
--- working_with_controller.py
+++ working_with_controller.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame_example
 
 pygame.init()
 
